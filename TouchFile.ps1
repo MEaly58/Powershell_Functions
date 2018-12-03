f1840afdbaa53ddaf2748425c1fe05867fcb903a
Mathew Ealy <Mealy.IT@Gmail.com>
2018-12-03 12:28:06 -0500

    Create TouchFile.ps1

---
+++ TouchFile.ps1
@@ -0,0 +1,19 @@
+<#
+Fucntion to update the last write date/time of a file.  
+#>
+Function Touch-File
+{
+    $file = $args[0]
+    if($file -eq $null) {
+        throw "No filename supplied"
+    }
+
+    if(Test-Path $file)
+    {
+        (Get-ChildItem $file).LastWriteTime = Get-Date
+    }
+    else
+    {
+        echo $null > $file
+    }
+}
